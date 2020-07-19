f258ad7e12d4ae6c7527452654ef5c1ea0d2b3a8
Jah <kennedye2@vcu.edu>
2020-07-19 14:54:47 -0500

    gutted extraneous events called in the index.html as well to prepare for vue refactoring

--- public/app.js
+++ public/app.js
@@ -3,11 +3,9 @@ var map;
 document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
     const db = firebase.firestore();
-    // do something with db upon startup
 });
 
-function initMap() {
-  // map variable has been initialized globally
+function initMap() {   // map variable has been initialized globally
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
