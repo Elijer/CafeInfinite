91840cdd6f8841a97fb310bbfac0efe6420e29dc
Jah <kennedye2@vcu.edu>
2020-08-10 14:06:45 -0500

    npm test now runs firebase emulators, watchify, and builds source maps with excorcist

--- public/bundle.js
+++ public/bundle.js
@@ -8,24 +8,30 @@ document.addEventListener("DOMContentLoaded", event => {
 
 
 // Create the script tag, set the appropriate attributes
-var script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCcFvCdo_5DPAUfRdhVq8sttNxwLAhLpUI&callback=initMap';
-script.defer = true;
 
-// Attach your callback function to the `window` object
-window.initMap = function() {
+function setupMap(){
+  var script = document.createElement('script');
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCcFvCdo_5DPAUfRdhVq8sttNxwLAhLpUI&callback=initMap';
+  script.defer = true;
+  
+  // Attach your callback function to the `window` object
+  window.initMap = function() {
+  
+    var IndianaPA = {lat: 40.6215, lng: -79.1525}
+  
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: IndianaPA,
+      zoom: 16
+    });
+  };
+  
+  // Append the 'script' element to 'head'
+  document.head.appendChild(script);
+}
 
-  var IndianaPA = {lat: 40.6215, lng: -79.1525}
+setupMap();
 
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: IndianaPA,
-    zoom: 16
-  });
-};
-
-// Append the 'script' element to 'head'
-document.head.appendChild(script);
-
+//browserify public/app.js --debug | exorcist public/bundle.map.js > public/bundle.js
 
 /*
 var IndianaPA = {lat: 40.6215, lng: -79.1525}
@@ -37,4 +43,5 @@ window.intiMap = function() {   // map variable has been initialized globally
   });
 }
 */
-},{}]},{},[1]);
+},{}]},{},[1])
+//# sourceMappingURL=bundle.map.js
