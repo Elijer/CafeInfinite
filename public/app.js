2f6eab20dd1a1f72b6ac52f84449da8f5e2e8fe6
Jah <kennedye2@vcu.edu>
2020-08-28 15:53:23 -0400

    added firebaseConfig into own folder for brevity and security

--- public/app.js
+++ public/app.js
@@ -1,32 +1,7 @@
-/* New code
-let newMap = require('./components/geo/newMap');
-let test = require('./components/test');
-
-document.addEventListener("DOMContentLoaded", event => {
-    //const app = firebase.app();
-    //const db = firebase.firestore();
-    console.log("dom is loaded");
-    test();
-});
-
-newMap();
-*/
-
 var firebase = require('firebase/app');
-var firestore = require('firebase/firestore');
+var firestore = require('firebase/firestore'); // yes this is needed
+var firebaseConfig = require('./firebaseConfig');
 
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCNpjyVF33s7frX9uCWYqur4DfRaizx3vg",
-    authDomain: "cafe-infinite.firebaseapp.com",
-    databaseURL: "https://cafe-infinite.firebaseio.com",
-    projectId: "cafe-infinite",
-    storageBucket: "cafe-infinite.appspot.com",
-    messagingSenderId: "197502459222",
-    appId: "1:197502459222:web:22ce4f978d4b649b284af7",
-    measurementId: "G-R6GFMFJWGW"
-  };
-  
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
 db.collection("users").add({
@@ -49,3 +24,19 @@ var geo     =       require('./geographicFunctionality/1__geo');
 geo();
 
 // browserify --debug public/app.js -o public/bundle.js
+
+
+
+/* New code
+let newMap = require('./components/geo/newMap');
+let test = require('./components/test');
+
+document.addEventListener("DOMContentLoaded", event => {
+    //const app = firebase.app();
+    //const db = firebase.firestore();
+    console.log("dom is loaded");
+    test();
+});
+
+newMap();
+*/
---
+++ public/firebaseConfig.js
@@ -0,0 +1,12 @@
+firebaseConfig = {
+    apiKey: "AIzaSyCNpjyVF33s7frX9uCWYqur4DfRaizx3vg",
+    authDomain: "cafe-infinite.firebaseapp.com",
+    databaseURL: "https://cafe-infinite.firebaseio.com",
+    projectId: "cafe-infinite",
+    storageBucket: "cafe-infinite.appspot.com",
+    messagingSenderId: "197502459222",
+    appId: "1:197502459222:web:22ce4f978d4b649b284af7",
+    measurementId: "G-R6GFMFJWGW"
+};
+
+module.exports = firebaseConfig;
