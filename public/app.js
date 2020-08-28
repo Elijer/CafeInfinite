6e162edae0d03194787d34344ecbc52dbe945027
Jah <kennedye2@vcu.edu>
2020-08-28 15:48:14 -0400

    added firebase and firestore

--- public/app.js
+++ public/app.js
@@ -2,23 +2,50 @@
 let newMap = require('./components/geo/newMap');
 let test = require('./components/test');
 
+document.addEventListener("DOMContentLoaded", event => {
+    //const app = firebase.app();
+    //const db = firebase.firestore();
+    console.log("dom is loaded");
+    test();
+});
+
 newMap();
 */
 
-const functions = require('firebase-functions');
-const admin = require('firebase-admin'); //const { firestore } = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+var firebase = require('firebase/app');
+var firestore = require('firebase/firestore');
 
-let db = admin.firestore();
-let db2 = db.collection('flames'); //let db1 = db.collection('products');
-var geo = require('./geographicFunctionality/1__geo');
 
-document.addEventListener("DOMContentLoaded", event => {
-    const app = firebase.app();
-    const db = firebase.firestore();
-    console.log("dom is loaded");
-    geo(db2);
-    //test();
+const firebaseConfig = {
+    apiKey: "AIzaSyCNpjyVF33s7frX9uCWYqur4DfRaizx3vg",
+    authDomain: "cafe-infinite.firebaseapp.com",
+    databaseURL: "https://cafe-infinite.firebaseio.com",
+    projectId: "cafe-infinite",
+    storageBucket: "cafe-infinite.appspot.com",
+    messagingSenderId: "197502459222",
+    appId: "1:197502459222:web:22ce4f978d4b649b284af7",
+    measurementId: "G-R6GFMFJWGW"
+  };
+  
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+db.collection("users").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
 });
+
+
+
+
+var geo     =       require('./geographicFunctionality/1__geo');
+
+geo();
 
 // browserify --debug public/app.js -o public/bundle.js
