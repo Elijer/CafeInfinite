eb5117f319672826bf77ad08b78dc937c58d4ffd
Jah <kennedye2@vcu.edu>
2020-09-16 00:52:44 -0400

    changed createFlame to createBeacon

--- public/geographicFunctionality/mapClick.js
+++ public/geographicFunctionality/mapClick.js
@@ -1,18 +1,17 @@
 var $                   = require('jquery');
 //var rootURL             = require ('../rootURL');
-var createFlame         = require('./createFlame');
+var createFlame         = require('./createBeacon');
 var gifs                = require('./common/gif_library');
 
 var flame = "./geographicFunctionality/gifs/flames/flame.gif";
 
 var mapClick = function(googleMaps, db){
     map.addListener('click', function(e) {
-    //google maps stores click lat & lng in a weird way;
-    var lat = e.latLng.lat();
-    var lng = e.latLng.lng();
-    //console.log("their style", lat, lng)
 
-    createFlame(googleMaps, lat, lng, gifs.flame, db/*, rootURL*/);
+    var lat = e.latLng.lat(); //google maps stores click lat & lng in a weird way;
+    var lng = e.latLng.lng();
+
+    createBeacon(googleMaps, lat, lng, gifs.flame, db/*, rootURL*/);
 
   });
 };
