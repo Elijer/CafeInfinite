6939c2409a4f3c09f71388574bbf02a9f937bc5b
Jah <kennedye2@vcu.edu>
2020-09-17 22:23:08 -0400

    made icon choices bigger on mobile

--- public/views/markerMenu.js
+++ public/views/markerMenu.js
@@ -3,13 +3,11 @@ store.mapClick.active = false; */
 
 function markerMenu(){
     var box = document.getElementById("selection-box");
-
     if (box.style.display == "inline"){
         box.style.display = "none";
     } else {
         box.style.display = "inline";
     }
-
 }
 
 module.exports = {markerMenu}
