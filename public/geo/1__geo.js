3143f6dd1a487d3f45ffa769a165d7aef4c67608
Jah <kennedye2@vcu.edu>
2020-09-20 20:42:14 -0400

    trivial

--- public/geo/1__geo.js
+++ public/geo/1__geo.js
@@ -72,12 +72,5 @@ function geo(db, key){
 
 };
 
-/*       var lat = parseFloat(localStorage.getItem('lat'));
-      var lng = parseFloat(localStorage.getItem('lng'));
-      if (lat && lng){
-        createBeacon(googleMaps, lat, lng, gifs.flame, db);
-      }
- */
-
 
 module.exports = geo;
