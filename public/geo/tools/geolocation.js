e1b6a9e047af40e98d198c25c1ac1eb37ec312ec
Jah <kennedye2@vcu.edu>
2020-11-05 16:46:54 -0500

    added new psuedo-coded functions in geolocation.js

--- public/geo/tools/geolocation.js
+++ public/geo/tools/geolocation.js
@@ -48,4 +48,26 @@ var geolocation = function(){
 
 };
 
+var watchPosition = function(){
+  // This functionc an be called one time when map is initiated in order to watch the user's GPS location and save it
+  // to a global variable. That way, when a new post is made, it can simply consult the store
+  // One concern I have with this is, how often does the watch method update? Can the update rate be trusted?
+}
+
+var getPosition = function(callback){
+  // In case watch position can't be trusted, this can be used instead to get the user's location as requested.
+  // This may be ideal, as it only consults geolocation when needed, potentially increasing performance and,
+  // more importantly, providing more accurate geolocation information. However, one downside is that it could cause a lag
+  // between the position request and when it is actually delivered.
+  // It might be better to simply set the marker's position to whatever is in the store, and THEN change it to what
+  // getPosition returns, for the best of both worlds.
+  // Note: both getPosition and watchPosition should be used WITH centerMap() so that user is 'guided' to their current
+  // position when they make a post. OR, they could be guided there when they press the New Marker Button, instead.
+}
+
+
+var centerMap = function(){
+
+}
+
 module.exports = { geolocation };
